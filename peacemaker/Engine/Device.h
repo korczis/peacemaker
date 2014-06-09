41215575cd0ca2e81d8b88a0c1a05389a7d965e3
Tomas Korcak <korczis@gmail.com>
2014-06-08 18:36:45 -0700

    Added MIT license

--- peacemaker/Engine/Device.h
+++ peacemaker/Engine/Device.h
@@ -2,9 +2,27 @@
 //  Device.h
 //  peacemaker
 //
-//  Created by Tomas Korcak on 6/8/14.
-//  Copyright (c) 2014 Tomas Korcak. All rights reserved.
+// The MIT License (MIT)
 //
+// Copyright (c) 2014 Tomas Korcak <korczis@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #ifndef __peacemaker__Engine_Device__
 #define __peacemaker__Engine_Device__
@@ -21,37 +39,62 @@ namespace pm
    class Device
    {
    public:
+      /**
+       * @brief Constructor
+       */
       Device(const std::string& name = "", const std::string& description = "");
+      
+      /**
+       * @brief Destructor
+       */
       virtual ~Device();
       
       typedef std::shared_ptr<Capture> CapturePtr;
       
+      /**
+       * @brief Create default capture instance
+       */
       inline CapturePtr CreateCapture() const
       {
          return CreateCapture<Capture>();
       }
       
+      /**
+       * @brief Create specific capture instance
+       */
       template<typename T>
       inline std::shared_ptr<T> CreateCapture() const
       {
          return std::make_shared<T>(this->getName().c_str());
       }
       
+      /**
+       * Get device description
+       */
       inline const std::string& getDescription() const
       {
          return mDescription;
       }
       
+      /**
+       * @brief Set device description
+       */
       inline Device& setDescription(const std::string& description) {
          mDescription = description;
          return *this;
       }
       
+      /**
+       * @brief Get device name
+       */
       inline const std::string& getName() const
       {
          return mName;
       }
       
+      /**
+       * @brief Set device name
+       */
       inline Device& setName(const std::string& name) {
          mName = name;
          return *this;
--- peacemaker/Engine/Engine.h
+++ peacemaker/Engine/Engine.h
@@ -2,15 +2,34 @@
 //  Engine.h
 //  peacemaker
 //
-//  Created by Tomas Korcak on 6/8/14.
-//  Copyright (c) 2014 Tomas Korcak. All rights reserved.
+// The MIT License (MIT)
 //
+// Copyright (c) 2014 Tomas Korcak <korczis@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #ifndef __peacemaker__Engine__
 #define __peacemaker__Engine__
 
 // core c++ includes
 #include <memory>
+#include <set>
 #include <vector>
 
 namespace pm
@@ -21,25 +40,45 @@ namespace pm
    class Engine
    {
    public:
+      /**
+       * @brief Constructor
+       */
       Engine();
+      
+      /**
+       * @brief destructor
+       */
       virtual ~Engine();
       
       typedef std::shared_ptr<Capture> CapturePtr;
       typedef std::vector<Capture> CaptureList;
       typedef std::vector<CapturePtr> CapturePtrList;
+      typedef std::set<CapturePtr> CaptureSet;
       
       typedef std::shared_ptr<Device> DevicePtr;
       typedef std::vector<Device> DeviceList;
       typedef std::vector<DevicePtr> DevicePtrList;
       
+      /**
+       * @brief Add capture to set managed by engine
+       */
       inline Engine& AddCapture(CapturePtr capture)
       {
-      	mCaptures.push_back(capture);
+      	mCaptures.insert(capture);
          return *this;
       }
       
       /**
-       * Get device by name
+       * @brief Remove capture from set managed by engine
+       */
+      inline Engine& RemoveCapture(CapturePtr capture)
+      {
+      	mCaptures.erase(capture);
+         return *this;
+      }
+      
+      /**
+       * @brief Get device by name
        */
       const DevicePtr GetDevice(const char* name = NULL) const;
       
@@ -74,6 +113,9 @@ namespace pm
        */
       static size_t ListDevices(DevicePtrList& devices, bool include_any = true);
       
+      /**
+       * @brief Run engine loop
+       */
       virtual int Loop();
       
       /**
@@ -82,7 +124,7 @@ namespace pm
       void Sniff(const std::string& device);
       
    private:
-      CapturePtrList mCaptures;
+      CaptureSet mCaptures;
       DevicePtrList mDevices;
    };
 }; /* namespace pm */
