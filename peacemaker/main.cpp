fa8c3340af3ead1f066edd29d84374930967750b
Tomas Korcak <korczis@gmail.com>
2014-06-08 19:52:51 -0700

    TestCapture ...

--- peacemaker/main.cpp
+++ peacemaker/main.cpp
@@ -40,6 +40,9 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread.hpp>
 
+// pcap includes
+#include <pcap.h>
+
 auto Logger = pm::Logger::Instance();
 
 class TestCapture : public pm::Capture
@@ -49,9 +52,82 @@ public:
    {
    }
    
+   /* 4 bytes IP address */
+   typedef struct ip_address{
+      u_char byte1;
+      u_char byte2;
+      u_char byte3;
+      u_char byte4;
+   } ip_address;
+   
+   /* IPv4 header */
+   typedef struct ip_header{
+      u_char  ver_ihl;        // Version (4 bits) + Internet header length (4 bits)
+      u_char  tos;            // Type of service
+      u_short tlen;           // Total length
+      u_short identification; // Identification
+      u_short flags_fo;       // Flags (3 bits) + Fragment offset (13 bits)
+      u_char  ttl;            // Time to live
+      u_char  proto;          // Protocol
+      u_short crc;            // Header checksum
+      ip_address  saddr;      // Source address
+      ip_address  daddr;      // Destination address
+      u_int   op_pad;         // Option + Padding
+   } ip_header;
+   
+   /* UDP header*/
+   typedef struct udp_header{
+      u_short sport;          // Source port
+      u_short dport;          // Destination port
+      u_short len;            // Datagram length
+      u_short crc;            // Checksum
+   } udp_header;
+
+   
    virtual void Callback(const struct pcap_pkthdr *header, const unsigned char *data)
    {
-      pm::Capture::Callback(header, data);
+      // pm::Capture::Callback(header, data);
+      
+      struct tm *ltime;
+      char timestr[16];
+      ip_header *ih = NULL;
+      udp_header *uh = NULL;
+      u_int ip_len;
+      u_short sport,dport;
+      time_t local_tv_sec;
+      
+      /* convert the timestamp to readable format */
+      local_tv_sec = header->ts.tv_sec;
+      ltime=localtime(&local_tv_sec);
+      strftime( timestr, sizeof timestr, "%H:%M:%S", ltime);
+      
+      /* print timestamp and length of the packet */
+      printf("%s.%.6d len:%d ", timestr, header->ts.tv_usec, header->len);
+      
+      /* retireve the position of the ip header */
+      ih = (ip_header *) (data + 14); //length of ethernet header
+      
+      /* retireve the position of the udp header */
+      ip_len = (ih->ver_ihl & 0xf) * 4;
+      uh = (udp_header *) ((u_char*)ih + ip_len);
+      
+      /* convert from network byte order to host byte order */
+      sport = ntohs(uh->sport);
+      dport = ntohs(uh->dport);
+      
+      /* print ip addresses and udp ports */
+      printf("%d.%d.%d.%d:%d -> %d.%d.%d.%d:%d\n",
+			ih->saddr.byte1,
+			ih->saddr.byte2,
+			ih->saddr.byte3,
+			ih->saddr.byte4,
+			sport,
+			ih->daddr.byte1,
+			ih->daddr.byte2,
+			ih->daddr.byte3,
+			ih->daddr.byte4,
+			dport
+		);
    }
 };
 
